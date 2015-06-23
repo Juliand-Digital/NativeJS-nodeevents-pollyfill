1a760c50c4dc03cb7b6160cc514bdaf73b5bde67
rjmilnes <richard@juliand.co.uk>
2015-06-23 17:18:50 +0100

    Fixed context issue with context var

--- nodejs-events-polyfill.js
+++ nodejs-events-polyfill.js
@@ -1,23 +1,23 @@
  
 /* Polyfill EventEmitter - native JS implementation of node event library */
-var EventEmitter = function (context) {};
+var EventEmitter = function (context) {this.context = context};
  
 EventEmitter.prototype.on = function (event, listener) {
-    context.addEventListener(event, listener, false);
+    this.context.addEventListener(event, listener, false);
 };
  
 EventEmitter.prototype.removeListener = function (event, listener) {
-    context.removeEventListener(event,listener);
+    this.context.removeEventListener(event,listener);
 };
  
 EventEmitter.prototype.emit = function (name) {
-    if (context.dispatchEvent) {
+    if (this.context.dispatchEvent) {
         var evt = new Event(name);
-        context.dispatchEvent(evt);
+        this.context.dispatchEvent(evt);
     } else {  /* Catch for older implementations */  
         var evt = document.createEvent("Event");
         evt.initEvent(name,true,true);
-        context.dispatchEvent(evt);
+        this.context.dispatchEvent(evt);
     }
 };
 
